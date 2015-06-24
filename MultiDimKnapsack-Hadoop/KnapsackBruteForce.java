b25740af94d9c053077771d07d05cbee8718236e
Reza Rahimi <mohamadreza.rahimi@gmail.com>
2015-06-24 10:10:40 -0700

    Update KnapsackBruteForce.java

--- MultiDimKnapsack-Hadoop/KnapsackBruteForce.java
+++ MultiDimKnapsack-Hadoop/KnapsackBruteForce.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
